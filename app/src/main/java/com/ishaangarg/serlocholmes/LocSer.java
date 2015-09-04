249aca15f7dcdb94745043263983d0e157c80a90
ishaansrm <ishaan_garg@srmuniv.edu.in>
2015-09-04 20:31:08 +0530

    DONE

--- app/src/main/java/com/ishaangarg/serlocholmes/LocSer.java
+++ app/src/main/java/com/ishaangarg/serlocholmes/LocSer.java
@@ -1,112 +1,151 @@
 package com.ishaangarg.serlocholmes;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-public class LocSer extends Service {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class LocSer extends Service implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     final static String TAG = "LocSer";
 
-    private LocationManager mLocationManager = null;
-    private static final int LOCATION_INTERVAL = 10;
-    private static final float LOCATION_DISTANCE = 0f;
+    Location mCurrentLocation;
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    static Location lastLoc = new Location("PrevLoc");
 
-    private class LocationListener implements android.location.LocationListener{
-        Location mLastLocation;
-        public LocationListener(String provider)
-        {
-            Log.d(TAG, "LocationListener " + provider);
-            mLastLocation = new Location(provider);
-        }
-        @Override
-        public void onLocationChanged(Location location)
-        {
-            Log.d(TAG, "onLocationChanged: " + location);
-            mLastLocation.set(location);
-        }
-        @Override
-        public void onProviderDisabled(String provider) {
-            Log.d(TAG, "onProviderDisabled: " + provider);
-        }
-        @Override
-        public void onProviderEnabled(String provider) {
-            Log.d(TAG, "onProviderEnabled: " + provider);
-        }
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras)
-        {
-            Log.d(TAG, "onStatusChanged: " + provider);
-        }
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(30000); //30 seconds
+        mLocationRequest.setFastestInterval(30000); //30 seconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
-    LocationListener[] mLocationListeners = new LocationListener[] {
-            new LocationListener(LocationManager.GPS_PROVIDER),
-            new LocationListener(LocationManager.NETWORK_PROVIDER)
-    };
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
-        Log.d(TAG, "onStartCommand");
-        super.onStartCommand(intent, flags, startId);
-        return START_STICKY;
+    public void onConnected(Bundle connectionHint) {
+        Log.d(TAG, "CONNECTED");
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        createLocationRequest();
+        startLocationUpdates();
     }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    String mLastUpdateTime;
+    static Boolean stale = true;
+
     @Override
-    public void onCreate()
-    {
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "Last LOC: " + lastLoc.getLatitude() + " | " + lastLoc.getLongitude());
+        Log.d(TAG, "New LOC: " + location.getLatitude() + " | " + location.getLongitude());
+
+        if (location.distanceTo(lastLoc) > 100 || stale) {
+            if (stale)
+                Log.d(TAG, "Forcibly polling location");
+            mCurrentLocation = location;
+            mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+            updateUI();
+            stale = false;
+        } else {
+            Log.d(TAG, "Not moved 100 meters");
+            stale = true;
+        }
+        lastLoc = location;
+    }
+
+    private void updateUI() {
+
+        Log.d(TAG, "Moved >100 meters");
+
+        String locStr = mLastUpdateTime + ": " + mCurrentLocation.getLatitude() + ", " + mCurrentLocation.getLongitude();
+
+        Intent intent = new Intent("DrWatson");
+        intent.putExtra("loc", locStr);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+
+        Log.d(TAG, "Time: " + mLastUpdateTime + " | Lat: "
+                + String.valueOf(mCurrentLocation.getLatitude()) + " | Long: "
+                + String.valueOf(mCurrentLocation.getLongitude()));
+    }
+
+    @Override
+    public void onCreate() {
         Log.d(TAG, "onCreate");
-        initializeLocationManager();
-        try {
-            mLocationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
-                    mLocationListeners[1]);
-        } catch (java.lang.SecurityException ex) {
-            Log.d(TAG, "fail to request location update, ignore", ex);
-        } catch (IllegalArgumentException ex) {
-            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
-        }
-        try {
-            mLocationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
-                    mLocationListeners[0]);
-        } catch (java.lang.SecurityException ex) {
-            Log.d(TAG, "fail to request location update, ignore", ex);
-        } catch (IllegalArgumentException ex) {
-            Log.d(TAG, "gps provider does not exist " + ex.getMessage());
-        }
-    }
-    @Override
-    public void onDestroy()
-    {
-        Log.d(TAG, "onDestroy");
-        super.onDestroy();
-        if (mLocationManager != null) {
-            for (int i = 0; i < mLocationListeners.length; i++) {
-                try {
-                    mLocationManager.removeUpdates(mLocationListeners[i]);
-                } catch (Exception ex) {
-                    Log.d(TAG, "failed to remove location listeners, ignore", ex);
-                }
-            }
-        }
-    }
-    private void initializeLocationManager() {
-        Log.d(TAG, "initializeLocationManager");
-        if (mLocationManager == null) {
-            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-        }
+
+        lastLoc.setLatitude(0);
+        lastLoc.setLongitude(0);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mGoogleApiClient.connect();
     }
 
-    public LocSer() {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("SerLock")
+                .setContentText("SerLock has your back, I mean your Location")
+                .setContentIntent(pendingIntent).build();
+
+        startForeground(47, notification);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection Failed: " + connectionResult);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopLocationUpdates();
     }
 }
--- app/src/main/java/com/ishaangarg/serlocholmes/MainActivity.java
+++ app/src/main/java/com/ishaangarg/serlocholmes/MainActivity.java
@@ -1,12 +1,18 @@
 package com.ishaangarg.serlocholmes;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,29 +21,25 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, LocSer.class);
         startService(intent);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("DrWatson"));
+
+        tv = (TextView) findViewById(R.id.text_view);
     }
 
-
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = intent.getStringExtra("loc");
+            Log.d("receiver", "Got message: " + message);
+            tv.append("\n\n" + message);
+        }
+    };
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        super.onDestroy();
+    }
 }
